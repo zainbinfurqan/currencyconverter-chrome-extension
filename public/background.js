81f39541f9fd84c79f085f40ea03f7eca95ed989
zain ahmed <zain.ahmed199524@gmail.com>
2024-11-21 01:26:20 +0800

    update code

--- public/background.js
+++ public/background.js
@@ -1,5 +1,5 @@
 if(typeof process !== 'undefined' && process.env){
-const apiKey = process.env.REACT_APP_EXCHANGE_RATE_API_KEY;  // This should be bundled via Webpack
+const apiKey = process.env.REACT_APP_GOOGLE_GEMINI_KEY;  // This should be bundled via Webpack
 chrome.runtime.onInstalled.addListener(() => {
   console.log("API Key from background: ", apiKey);
 });
