237ce6e6674bc0f3564e55ec9dc44059c7c9d0f7
zain ahmed <zain.ahmed199524@gmail.com>
2024-11-20 22:03:48 +0800

    update code with some fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import './App.css';
 import { useEffect, useState } from 'react';
+// import ReactLoader from 'react-single-loader'
+// import 'react-single-loader/dist/index.css'
+import { Audio } from 'react-loader-spinner'
 
 function App() {
 
@@ -11,6 +14,7 @@ function App() {
   const [defaultSelectedCurrency , setDefaultSelectedCurrency] = useState<any>("USD")
   const [showCurrencyDropDown, setShowCurrencyDropDown] = useState<any>(false)
   const [filteredCurrencyConvertedList, setFilteredCurrencyConvertedList] = useState<any>({})
+  const [validAmount,setValidAmount] = useState<any>(false)
   const [error,setError] = useState<any>({
     amount:null,
     currencyCode: null
@@ -21,13 +25,35 @@ function App() {
     content:''
   })
 
-  const convertCurrency =  async () => {
+const checkValidCurrencyInputField = (value: any) => {
+  if(/^[1-9][0-9]*(\.[0-9]+)?$/.test(value)){
+    setValidAmount(/^[1-9][0-9]*(\.[0-9]+)?$/.test(value))
+    setAmount(value)
+    setError({amount:null,currencyCode:null})
+    }else{
+      setValidAmount(/^[1-9][0-9]*(\.[0-9]+)?$/.test(value))
+      setError({amount:'Please enter valid amount',currencyCode:null})
+    }
+}
+
+const checkValidFilterInputField = (value: any) => {
+  if(/^([A-Z]{3})(,([A-Z]{3}))*$/.test(value)){
+    setValidAmount(/^([A-Z]{3})(,([A-Z]{3}))*$/.test(value))
+    setCurrencyCode(value)
+    setError({amount:null,currencyCode:null})
+    }else{
+    setCurrencyCode(value)
+    setValidAmount(/^([A-Z]{3})(,([A-Z]{3}))*$/.test(value))
+    setError({amount:null,currencyCode:'Please enter valid currency code'})
+  }
+}
+
+  const convertCurrency =  async (item: any) => {
     try {
-      if(/^[1-9][0-9]*$/.test(amount))
-      {
+      if(validAmount) {
         setError({amount:null,currencyCode:null})
         setIsLoading(true)
-        const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.REACT_APP_EXCHANGE_RATE_API_KEY}/latest/${defaultSelectedCurrency}`)
+        const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.REACT_APP_EXCHANGE_RATE_API_KEY}/latest/${item}`)
         const res = await response.json()
         
         const response_ = await fetch('https://restcountries.com/v3.1/all');
@@ -50,7 +76,6 @@ function App() {
     })
     setConversionToAllCurrency(conversionToAllCurrency)
   }else{
-    setError({amount:'Please enter valid amount',currencyCode:null})
   }
     } catch (error) {
       console.log(error)
@@ -70,27 +95,31 @@ function App() {
       setIsFilter(true)
       setIsLoading(false)
   }else{
-    setError({amount:null,currencyCode:'Please enter valid currency code'})
   }
   }
   return (
     <div className="App font-Outfit">
-    {/* {isLoading && <ReactLoader color="blue"/>} */}
+    {isLoading && <div className='loader'><Audio
+        height="80"
+        width="80"
+        color="#2463EB"
+        ariaLabel="loading"
+      /></div>}
       <div className='p-4'>
       <div className="max-w-xl mx-auto">
         <div className="flex flex-row max-[425px]:flex-col ">
             <div className="relative w-full">
               <div>
-                <p onClick={Object.keys(conversionToAllCurrency).length > 0 ?()=>setShowCurrencyDropDown(!showCurrencyDropDown) : ()=>{}} className='absolute top-0 bg-blue-300 -left-1 border border-blue-300 h-full p-2.5 text-sm font-medium text-white  rounded-l-lg border '>{defaultSelectedCurrency}</p>
+                <p  className='absolute top-0 bg-blue-600 -left-1 border border-blue-600 h-full p-2.5 text-sm font-medium text-white  rounded-l-lg border '>{defaultSelectedCurrency}</p>
               </div>
-                <input type="search" onChange={(e)=>setAmount(e.target.value)} className="block p-2.5 pl-14 w-full z-20 text-sm text-gray-900 rounded-lg  border border-gray-300 focus:outline-none focus:border-blue-300 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-300" placeholder="Enter amount" required />
-                <button onClick={convertCurrency} type="submit"  className="absolute top-0 end-0 h-full p-2.5 text-sm font-medium text-white bg-blue-300 rounded-e-lg border border-blue-300 hover:bg-blue-300  focus:outline-none dark:outline-none  dark:hover:outline-none  dark:focus:outline-none ">
+                <input type="search" onChange={(e)=>checkValidCurrencyInputField(e.target.value)} className="block p-2.5 pl-14 w-full z-20 text-sm text-gray-900 rounded-lg  border border-gray-600 focus:outline-none focus:border-blue-300 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-300" placeholder="Enter amount" required />
+                <button onClick={()=>convertCurrency(defaultSelectedCurrency)} type="submit"  className="absolute top-0 end-0 h-full p-2.5 text-sm font-medium text-white bg-blue-600 rounded-e-lg border border-blue-600 hover:bg-blue-600  focus:outline-none dark:outline-none  dark:hover:outline-none  dark:focus:outline-none ">
                   <p>Convert</p>
                 </button>
             </div>
             {Object.keys(conversionToAllCurrency).length > 0 && 
               <div className='flex flex-col mx-2 max-[425px]:my-2 max-[425px]:items-center'>
-                <button onClick={()=>setShowCurrencyDropDown(!showCurrencyDropDown)} data-dropdown-toggle="dropdown" className="w-fit text-white bg-blue-300 hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-300 dark:focus:ring-blue-300" type="button">Currency
+                <button onClick={()=>setShowCurrencyDropDown(!showCurrencyDropDown)} data-dropdown-toggle="dropdown" className="w-fit text-white bg-blue-600 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-300 dark:focus:ring-blue-300" type="button">Currency
                   <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                     <path stroke="currentColor" stroke-linecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
                   </svg>
@@ -103,8 +132,8 @@ function App() {
       {Object.keys(conversionToAllCurrency).length >0 &&<div className="max-w-md mx-auto my-3">
         <div className="flex">
             <div className="relative w-full">
-                <input value={currencyCode} onChange={(e)=>setCurrencyCode(e.target.value)} type="search" className="block p-2.5 w-full z-20 text-sm text-gray-900 rounded-lg  border border-gray-300 focus:outline-none focus:border-blue-300 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-300" placeholder="Enter Currency Code(s)" required />
-                <button onClick={filter} type="submit" className="absolute top-0 end-0 h-full p-2.5 text-sm font-medium text-white bg-blue-300 rounded-e-lg border border-blue-300 hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-300 dark:focus:ring-blue-300">
+                <input value={currencyCode} onChange={(e)=>checkValidFilterInputField(e.target.value)} type="search" className="block p-2.5 w-full z-20 text-sm text-gray-900 rounded-lg  border border-gray-600 focus:outline-none focus:border-blue-600 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-300" placeholder="Enter Currency Code(s)" required />
+                <button onClick={filter} type="submit" className="absolute top-0 end-0 h-full p-2.5 text-sm font-medium text-white bg-blue-600 rounded-e-lg border border-blue-600 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-600 dark:bg-blue-600 dark:hover:bg-blue-300 dark:focus:ring-blue-300">
                   <p>Filter</p>
                 </button>
             </div>
@@ -121,7 +150,8 @@ function App() {
               <div className='flex row  justify-between items-center m-2'>
                 <h5 className="m-0 text-xl font-medium tracking-tight text-gray-900 dark:text-white font-Outfit">{item}</h5>
               </div>
-                <p className="mx-2 font-normal text-gray-700 dark:text-gray-400">{conversionToAllCurrency[item].toFixed(3)}</p>
+                {conversionToAllCurrency[item] && <p className="mx-2 font-normal text-gray-700 dark:text-gray-400">
+                  {conversionToAllCurrency[item].toFixed(3)}</p>}
             </div>
           )
         })}
@@ -131,19 +161,20 @@ function App() {
     <div className="relative p-4 w-full max-w-2xl max-h-full m-auto">
         <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <div className="flex items-center justify-between p-2 md:p-2 border-b rounded-t dark:border-gray-600">
-                <p className='font-bold mx-2'>Select country code</p>
+                <p className='font-bold mx-2 text-black'>Select country code</p>
                 <button onClick={()=>setShowCurrencyDropDown(!showCurrencyDropDown)} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
                     <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                     </svg>
                 </button>
             </div>
-            <div className="p-4 flex flex-wrap row">
+            <div className="p-4 flex flex-wrap row text-black">
             {Object.keys(conversionToAllCurrency).map((item, index)=>
                 <p onClick={()=>{
                   setShowCurrencyDropDown(!showCurrencyDropDown)
                   setDefaultSelectedCurrency(item)
                   setCurrencyCode('')
+                  convertCurrency(item)
                 }} onMouseOver={()=> { currencyToCountryCode[item] && setShowTooleTip({index,content:currencyToCountryCode[item].fullName}) }}
                 onMouseLeave={()=>setShowTooleTip({index:null,content:''})}
                 className="m-0  w-fit cursor-pointer px-2 self-center py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{item}
---
+++ src/react-single-loader.d.ts
@@ -0,0 +1,4 @@
+declare module 'react-single-loader' {
+    const value: any;
+    export default value;
+  }
