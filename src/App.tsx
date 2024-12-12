e0f120760e57b28d7bb9a47ac04b3109818d61f5
zain ahmed <zain.ahmed199524@gmail.com>
2024-12-12 23:30:14 +0800

    update website

--- src/App.tsx
+++ src/App.tsx
@@ -19,19 +19,19 @@ function App() {
         <div className='py-9'>
           <div className='flex self-center p-2'>
             <div>
-              <img height={30} width={30} src={AI} />
+              <img alt='' rel="noreferrer" height={30} width={30} src={AI} />
             </div>
             <p className='text-black text-lg self-center'>AI</p>
           </div>
           <div className='flex flex-wrap m-2'>
             <a target="_blank" href='https://chromewebstore.google.com/detail/content-reader/mcapgbonkmlfkkfbofiodakkiibfhhne'>
               <div className='shadow-md p-4 w-fit mr-2'>
-                <img height={60} width={60} src={ContentReader} />
+                <img alt='' rel="noreferrer" height={60} width={60} src={ContentReader} />
               </div>
             </a>
             <a target="_blank" href='https://chromewebstore.google.com/detail/language-translator/cccbggfgjgfihkneaigmjlbmppngecjn'>
               <div className='shadow-md p-4 w-fit'>
-                <img height={60} width={60} src={LanguageTranslator} />
+                <img alt='' rel="noreferrer" height={60} width={60} src={LanguageTranslator} />
               </div>
             </a>
           </div>
@@ -40,14 +40,14 @@ function App() {
         <div className='py-9'>
           <div className='flex self-center p-2'>
             <div>
-              <img height={30} width={30} src={Finance} />
+              <img alt='' rel="noreferrer" height={30} width={30} src={Finance} />
             </div>
             <p className='text-black text-lg self-center'>Finance</p>
           </div>
           <div className='flex flex-wrap m-2'>
             <a target="_blank" href='https://chromewebstore.google.com/detail/currency-converter-extens/dmclabfilpimdfkififkfndpgfkffbkm'>
               <div className='shadow-md p-4 w-fit'>
-                <img height={60} width={60} src={CurrencyConverter} />
+                <img alt='' rel="noreferrer" height={60} width={60} src={CurrencyConverter} />
               </div>
             </a>
           </div>
@@ -56,14 +56,14 @@ function App() {
         <div className='py-9'>
           <div className='flex self-center p-2'>
             <div>
-              <img height={30} width={30} src={Ecommerce} />
+              <img alt='' rel="noreferrer" height={30} width={30} src={Ecommerce} />
             </div>
             <p className='text-black text-lg self-center'>E-commerce</p>
           </div>
           <div className='flex flex-wrap m-2'>
             <a target="_blank" href='https://chromewebstore.google.com/detail/bookmark-your-favorites/mokpojcmdpjdgnfcgplidpmpanmloaoc'>
             <div className='shadow-md p-4 w-fit'>
-              <img height={60} width={60} src={SaveIt} />
+              <img alt='' rel="noreferrer" height={60} width={60} src={SaveIt} />
             </div>
             </a>
           </div>
