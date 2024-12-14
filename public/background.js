9e6ac6791a8fecaf52004c49f50a025edf73f922
zain ahmed <zain.ahmed199524@gmail.com>
2024-12-15 03:02:04 +0800

    update code

--- public/background.js
+++ public/background.js
@@ -1,13 +1,3 @@
-if (typeof process !== 'undefined' && process.env) {
-  const apiKey = process.env.REACT_APP_EXCHANGE_RATE_API_KEY; // This should be bundled via Webpack
-  chrome.runtime.onInstalled.addListener(() => {
-    console.log('API Key from background: ', apiKey);
-  });
-}
-chrome.runtime.onInstalled.addListener(() => {
-  // Send a pageview when the extension is installed
-  if (typeof ReactGA !== 'undefined') {
-    ReactGA.initialize('G-2Q2P0KWDK4'); // Use your GA4 Tracking ID
-    ReactGA.send('pageview', { dp: '/extension-install' }); // Custom page path
-  }
-});
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
--- src/App.tsx
+++ src/App.tsx
@@ -1,180 +1,162 @@
 import './App.css';
-import { useEffect, useState } from 'react';
-import { Audio } from 'react-loader-spinner';
-import ReactGA from 'react-ga4'; // Import react-ga4
-import List from './components/List';
-import { ConvertButton } from './components/ConvertButton';
-import { FilterButton } from './components/FilterButton';
-import { SearchInput } from './components/SearchInlut';
-import { SelectCurrencyModal } from './components/SelectCurrencyModal';
-
-const TRACKING_ID = 'G-2Q2P0KWDK4'; // Replace with your GA4 Tracking ID
+// import { useEffect, useState } from 'react';
+// import JSONPretty from 'react-json-pretty';
+// import 'react-json-pretty/themes/monikai.css';
+// import { JSONTree } from 'react-json-tree';
+import ReactJson from 'react-json-view'
 
 function App() {
-  const [currencyCode, setCurrencyCode] = useState<any>('');
-  const [isLoading, setIsLoading] = useState<any>(false);
-  const [amount, setAmount] = useState<any>('');
-  const [conversionToAllCurrency, setConversionToAllCurrency] = useState<any>({});
-  const [currencyToCountryCode, setCurrencyToCountryCode] = useState<any>({});
-  const [defaultSelectedCurrency, setDefaultSelectedCurrency] = useState<any>('USD');
-  const [showCurrencyDropDown, setShowCurrencyDropDown] = useState<any>(false);
-  const [filteredCurrencyConvertedList, setFilteredCurrencyConvertedList] = useState<any>({});
-  const [validAmount, setValidAmount] = useState<any>(false);
-  const [error, setError] = useState<any>({
-    amount: null,
-    currencyCode: null,
-  });
-  const [isFilter, setIsFilter] = useState<any>(false);
-  const [showTooleTip, setShowTooleTip] = useState<any>({
-    index: null,
-    content: '',
-  });
+  // const data_ = {}
 
-  useEffect(() => {
-    // Initialize Google Analytics
-    ReactGA.initialize(TRACKING_ID);
+  // const [data, setData] = useState<any>({});
+  // const [data_, setData_] = useState<any>({});
+  // const [isModalOpen, setIsModalOpen] = useState(false);
+  // const [key, setKey] = useState('');
+  // const [value, setValue] = useState('');
+  // const [isvValueObject, setIsvValueObject] = useState(false)
+  // const [isvValueString, setIsvValueString] = useState(false)
+  // const [isvValueNumber, setIsvValueNumber] = useState(false)
+  // const [nestedKey, setNestedKey] = useState('')
 
-    // Optionally track page view when the extension is opened
-    ReactGA.send('pageview');
-  }, []);
+  // const AddField = () => {
+  //     const newData = {...data};
+  //     if(nestedKey !== ''){
+  //     if(isvValueObject){
+  //       newData[nestedKey] = {...newData[nestedKey], [key]: JSON.parse('{}')};
+  //     }
+  //     if(isvValueNumber && /^\d+(\.\d+)?$/.test(value)) {
+  //       newData[nestedKey] =  {...newData[nestedKey], [key]: Number(value)};
+  //     }
+  //     if(isvValueString) {
+  //       newData[nestedKey] = {...newData[nestedKey], [key]:value};
+  //     }
+  //   } else {
+  //       if(isvValueObject){
+  //         newData[key] = JSON.parse('{}');
+  //       }
+  //       if(isvValueNumber && /^\d+(\.\d+)?$/.test(value)) {
+  //         newData[key] = Number(value);
+  //       }
+  //       if(isvValueString) {
+  //         newData[key] = value;
+  //       }
+  //     }
+  //       setIsvValueNumber(false)
+  //       setIsvValueString(false)
+  //       setIsvValueObject(false)
+  //       setValue('')
+  //       setKey('')
+  //       setData(newData)
+  //       setIsModalOpen(!isModalOpen)
+  //       setNestedKey('')
+  // }
 
-  const checkValidCurrencyInputField = (value: any) => {
-    if (/^[1-9][0-9]*(\.[0-9]+)?$/.test(value)) {
-      setValidAmount(/^[1-9][0-9]*(\.[0-9]+)?$/.test(value));
-      setAmount(value);
-      setError({ amount: null, currencyCode: null });
-    } else {
-      setValidAmount(/^[1-9][0-9]*(\.[0-9]+)?$/.test(value));
-      setError({ amount: 'Please enter valid amount', currencyCode: null });
-    }
-  };
+  // const onAdd = (data) => {
+    // console.log("data",data)
+    // setNestedKey(data)
+    // setIsModalOpen(!isModalOpen)
+  //   setKey(Object.keys(data.new_value)[0])
+  //  setIsModalOpen(!isModalOpen)
+  //   return false
+  // }
 
-  const checkValidFilterInputField = (value: any) => {
-    if (/^([A-Z]{3})(,([A-Z]{3}))*$/.test(value)) {
-      setValidAmount(/^([A-Z]{3})(,([A-Z]{3}))*$/.test(value));
-      setCurrencyCode(value);
-      setError({ amount: null, currencyCode: null });
-    } else {
-      setCurrencyCode(value);
-      setValidAmount(/^([A-Z]{3})(,([A-Z]{3}))*$/.test(value));
-      setError({
-        amount: null,
-        currencyCode: 'Please enter valid currency code',
-      });
-    }
-  };
+  // const RenderNestedObject = ({item, index}) => {
+  //   {console.log("data[item] typeof data[item] === 'object'",typeof data[item] === 'object')}
+  //   {console.log("data[item] data[item] !== null",data[item] !== null)}
+  //   {console.log("data[item] !Array.isArray(data[item])",!Array.isArray(data[item]))}
+  //   return <div key={index} className='flex flex-row'>
+  //     <p className='text-black'>{item}&nbsp;:&nbsp;</p> 
+     
+  //     {typeof data[item] === 'object' && data[item] !== null && !Array.isArray(data[item]) 
+  //       && (
+  //         <>
+  //           <p className='text-black text-lg self-center cursor-pointer' onClick={()=>onAdd(item)}>+</p>
+  //         </>
+  //       ) 
+  //     }
+  //     {typeof data[item] === 'object' && data[item] !== null && !Array.isArray(data[item]) && Object.keys(data[item]).map((item_,index_)=>{
+  //       return <RenderNestedObject item={item_} index={index_} />
+  //     })}
+  //   </div>
+    
+  // }
+   
+    const enableClipboardForRoot = (node) => {
+      // Root object will have no `name` or an empty `name`
+      return node && node.name === undefined;
+    };
 
-  const convertCurrency = async (item: any) => {
-    try {
-      if (validAmount) {
-        setError({ amount: null, currencyCode: null });
-        setIsLoading(true);
-        const response = await fetch(`${process.env.REACT_APP_EXCHANGE_BASE_URL}${process.env.REACT_APP_EXCHANGE_RATE_API_KEY}/latest/${item}`);
-        const res = await response.json();
-        const response_ = await fetch(process.env.REACT_APP_FLAG_IMAGES_URL || '');
-        const countries = await response_.json();
-        let currencyToCountryCode: any = {};
-
-        countries.forEach((country: any) => {
-          const currencies = country.currencies;
-          if (currencies) {
-            for (const [code, currency] of Object.entries(currencies)) {
-              currencyToCountryCode[code] = {
-                code: country.cca2.toLowerCase(),
-                fullName: country.name.common,
-              }; // cca2 is the country code
-            }
-          }
-        });
-        setCurrencyToCountryCode(currencyToCountryCode);
-        setFilteredCurrencyConvertedList(currencyToCountryCode);
-        const conversionToAllCurrency: any = {};
-        Object.keys(res?.conversion_rates).map((item: any) => {
-          conversionToAllCurrency[item] = res?.conversion_rates[item] * amount;
-        });
-        setConversionToAllCurrency(conversionToAllCurrency);
-      } else {
-      }
-    } catch (error) {
-      console.log(error);
-    }
-    setIsLoading(false);
-  };
-
-  const filter = () => {
-    if (/^([A-Z]{3})(,([A-Z]{3}))*$/.test(currencyCode)) {
-      setError({ amount: null, currencyCode: null });
-      setIsLoading(true);
-      const userSelectedCurrencyCode = currencyCode.split(',');
-      const filteredCurrencyConvertedList: any = {};
-      userSelectedCurrencyCode.map((item: any) => {
-        filteredCurrencyConvertedList[item.toUpperCase()] = conversionToAllCurrency[item.toUpperCase()];
-      });
-      setFilteredCurrencyConvertedList(filteredCurrencyConvertedList);
-      setIsFilter(true);
-      setIsLoading(false);
-    } else {
-    }
-  };
   return (
-    <div className="App font-Outfit">
-      {isLoading && (
-        <div className="loader">
-          <Audio height="80" width="80" color="#2463EB" ariaLabel="loading" />
-        </div>
-      )}
-      <div className="p-4">
-        <div className="max-w-xl mx-auto">
-          <div className="flex flex-row max-[425px]:flex-col ">
-            <SearchInput
-              checkValidCurrencyInputField={checkValidCurrencyInputField}
-              convertCurrency={convertCurrency}
-              defaultSelectedCurrency={defaultSelectedCurrency}
-            />
-            {Object.keys(conversionToAllCurrency).length > 0 && (
-              <ConvertButton setShowCurrencyDropDown={setShowCurrencyDropDown} showCurrencyDropDown={showCurrencyDropDown} />
-            )}
+    <div className="App font-Outfit px-2">
+      <div>
+       <p className='text-black text-md text-center py-4'>Create JSON Object</p>
+      </div>
+      {/* {Object.keys(data).keys.length == 0 && <div onClick={()=>setIsModalOpen(!isModalOpen)} className="text-black text-center">Add Field</div>} */}
+    {/*  */}
+    {/* <div id="default-modal" tabIndex={-1} aria-hidden="true" className={`${isModalOpen ? 'display' : 'hidden'} overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}>
+      <div className="relative p-4 w-full ">
+          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div className='flex flex-row p-2'>
+              <button onClick={()=>setIsModalOpen(!isModalOpen)} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+                  <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                  </svg>
+              </button>
+            </div>
+              <div className='p-2 flex flex-row justify-start'>
+                <div className='w-11/12 flex flex-row'>
+                <div className='text-left mr-4 '>
+                  <p className='text-black py-2'>Key</p>
+                  <input value={key} onChange={(e)=>setKey(e.target.value)} className='border text-black p-2' />
+                </div>
+                <div className='text-left'>
+                  <p className='text-black py-2'>Value</p>
+                  <input value={value} onChange={(e)=>setValue(e.target.value)} disabled={isvValueObject} className='border text-black  p-2' />
+                  <div className='flex flex-row py-2'>
+                    <div className='flex flex-row py-2 mr-2'>
+                      <input type='checkbox' disabled={isvValueString || isvValueNumber} checked={isvValueObject} onChange={()=>setIsvValueObject(!isvValueObject)}/>
+                      <p className='text-black text-xs ml-2'>Object</p>
+                    </div>
+                    <div className='flex flex-row py-2 mr-2'>
+                      <input type='checkbox' disabled={isvValueObject || isvValueNumber} checked={isvValueString} onChange={()=>setIsvValueString(!isvValueString)}/>
+                      <p className='text-black text-xs ml-2'>String</p>
+                    </div>
+                    <div className='flex flex-row py-2'>
+                      <input type='checkbox' disabled={isvValueObject || isvValueString} checked={isvValueNumber} onChange={()=>setIsvValueNumber(!isvValueNumber)}/>
+                      <p className='text-black text-xs ml-2'>Number</p>
+                    </div>
+                  </div>
+                </div>
+                </div>
+                <div className={`w-1/12 self-center ${isvValueNumber || isvValueObject || isvValueString && key && value ? 'display' : 'hidden'}`}>
+                  <p onClick={AddField} className='text-black text-lg cursor-pointer'>+</p>
+                </div>
+              </div>
           </div>
-          {error.amount != null && <p className="text-red-500 text-xs text-left">{error.amount}</p>}
-        </div>
-        {Object.keys(conversionToAllCurrency).length > 0 && (
-          <FilterButton currencyCode={currencyCode} checkValidFilterInputField={checkValidFilterInputField} filter={filter} error={error} />
-        )}
       </div>
-      <div className="flex flex-wrap overflow-scroll h-[77vh]">
-        {Object.keys(conversionToAllCurrency).length > 0 &&
-          Object.keys(isFilter ? filteredCurrencyConvertedList : conversionToAllCurrency).map(item => {
-            return (
-              currencyToCountryCode[item] !== undefined && (
-                <List
-                  code={item}
-                  amount={conversionToAllCurrency[item].toFixed(3)}
-                  isAvailable={conversionToAllCurrency[item]}
-                  imageCode={currencyToCountryCode[item].code}
-                />
+    </div> */}
+    {/*  */}
+
+      <div>
+        {/* {Object.keys(data).length > 0 &&
+          <div>
+            {Object.keys(data).map((item,index)=>{
+              return(
+                <RenderNestedObject item={item} index={index} />
               )
-            );
-          })}
+            })}
+          </div>
+        } */}
+        <ReactJson
+         style={{textAlign:'start'}} 
+          src={{}}       // Bind the state here
+          onAdd={()=>true}        // Update state when a new key is added
+          onEdit={()=>true}      // Update state when a key is edited
+          onDelete={()=>true}  // Update state when a key is deleted
+          enableClipboard={enableClipboardForRoot}
+        />
       </div>
-      <div
-        tabIndex={-1}
-        aria-hidden="true"
-        className={`${showCurrencyDropDown ? 'display' : 'hidden'}  scroll overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}
-      >
-        <div className="relative p-4 w-full max-w-2xl max-h-full m-auto">
-          <SelectCurrencyModal
-            setShowCurrencyDropDown={setShowCurrencyDropDown}
-            showCurrencyDropDown={showCurrencyDropDown}
-            conversionToAllCurrency={conversionToAllCurrency}
-            setDefaultSelectedCurrency={setDefaultSelectedCurrency}
-            convertCurrency={convertCurrency}
-            setCurrencyCode={setCurrencyCode}
-            setShowTooleTip={setShowTooleTip}
-            currencyToCountryCode={currencyToCountryCode}
-            showTooleTip={showTooleTip}
-          />
-        </div>
-      </div>
+
     </div>
   );
 }
